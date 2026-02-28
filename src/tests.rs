5125136a8f953f1ba8ed15fe223f1d67ae494649
wiltshire <wiltshire@ruf.st>
2026-02-28 19:06:03 +0200

    feat: `tokenize` function

--- src/tests.rs
+++ src/tests.rs
@@ -6,3 +6,17 @@ fn read_periodic_table() {
 	assert_eq!(periodic_table.elements.len(), 119);
 	assert_eq!(periodic_table.elements[49].name, "Tin");
 }
+
+#[test]
+fn tokenize() {
+	let string = "H2 + O2 = H2O";
+	let tokens = crate::token::tokenize(string);
+	println!("{:?}", tokens);
+}
+
+#[test]
+#[should_panic]
+fn tokenize_invalid_token() {
+	let string = "H2 + O/2 = H2O";
+	crate::token::tokenize(string).unwrap();
+}
--- src/token.rs
+++ src/token.rs
@@ -1,15 +1,29 @@
+use crate::element::Element;
 use crate::error::{LexerError, LexerResult};
 
 /// Enum representing possible chemical reaction expression tokens
 #[derive(Clone, Debug, PartialEq, PartialOrd)]
 pub enum Token {
+    /// Molecule coefficient
     Coefficient(isize),
-    /// Plus operator token
+    /// Element token
+    Element(String),
+    /// Subscript (index) token
+    Subscript(isize),
+    /// Plus (+) sign
     Plus,
-    /// Left bracket
+    /// Left bracket sign
     LeftBracket,
-    /// Right bracket
+    /// Right bracket sign
     RightBracket,
+    /// Left square bracket sign
+    LeftSquareBracket,
+    /// Right square bracket sign
+    RightSquareBracket,
+    /// Equals (=) sign
+    Equals,
+    /// End of expression
+    EOF
 }
 
 impl Token {
@@ -18,4 +32,34 @@ impl Token {
             _ => Err(LexerError::InvalidToken(kind.to_string())),
         }
     }
+}
+
+pub fn tokenize(input: &str) -> LexerResult<Vec<Token>> {
+    let mut tokens: Vec<Token> = Vec::new();
+    let mut iter = input.chars().peekable();
+
+    while let Some(ch) = iter.next() {
+        match ch {
+            ch if ch.is_whitespace() => continue,
+            '+' => tokens.push(Token::Plus),
+            '=' => tokens.push(Token::Equals),
+            '(' => tokens.push(Token::LeftBracket),
+            ')' => tokens.push(Token::RightBracket),
+            '[' => tokens.push(Token::LeftSquareBracket),
+            ']' => tokens.push(Token::RightSquareBracket),
+            '1'..='9' => {
+                let n: isize = std::iter::once(ch)
+                    .chain(std::iter::from_fn(|| iter.by_ref().next_if(|s| s.is_ascii_digit())))
+                    .collect::<String>()
+                    .parse()?;
+
+                tokens.push(Token::Coefficient(n));
+            },
+            'A'..='Z' | 'a'..='z' => tokens.push(Token::Element(ch.to_string())),
+            _ => return Err(LexerError::InvalidToken(String::from(ch)))
+        }
+    }
+
+    tokens.push(Token::EOF);
+    Ok(tokens)
 }
